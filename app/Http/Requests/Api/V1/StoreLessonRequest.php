254c2b9f1020ee7436c3d0d9d5ae25861eef2fc1
arya olya <99118866+aryaOlya@users.noreply.github.com>
2023-05-22 14:48:46 +0330

    Update StoreLessonRequest.php

--- app/Http/Requests/Api/V1/StoreLessonRequest.php
+++ app/Http/Requests/Api/V1/StoreLessonRequest.php
@@ -14,7 +14,7 @@ class StoreLessonRequest extends FormRequest
      * Determine if the user is authorized to make this request.
      */
 
-    public function __construct(array $query = [], array $request = [], array $attributes = [], array $cookies = [], array $files = [], array $server = [], $content = null,SettingRepository $settingRepository)
+    public function __construct(array $query = [], array $request = [], array $attributes = [], array $cookies = [], array $files = [], array $server = [], $content = null,?SettingRepository $settingRepository=null)
     {
         parent::__construct($query, $request, $attributes, $cookies, $files, $server, $content);
         $this->settingRepository = $settingRepository;
